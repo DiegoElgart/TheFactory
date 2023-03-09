a71d9d5ca55e8a6b745da90edca8b6972e4344bf
Diego Elgart <diego@Ipanel.local>
2023-03-09 11:23:31 +0200

    fixing some bugs

--- Server/middleware/actionsMiddleware.js
+++ Server/middleware/actionsMiddleware.js
@@ -1,26 +1,26 @@
 const actionsBLL = require("../BLL/actionsBLL");
 const dateUtil = require("../utils/dateSetter");
+const jwt = require("jsonwebtoken");
 
 const checkMaxActionsById = async (req, res, next) => {
     try {
-        const actionsForId = await actionsBLL.getAllActions();
-
-        const id = actionsForId[actionsForId.length - 1].id;
+        const token = req.header("access-Token");
+        const { id } = jwt.decode(token, process.env.SECRET_KEY);
+        console.log(id);
         const date = dateUtil.getDate();
         const actions = await actionsBLL.getActionsById(id);
         const actionsByDate = actions.filter(action => action.date === date);
-
         const len = actionsByDate.length - 1;
         if (actionsByDate[len].actionAllowed >= 1) {
             actionsByDate[len]["actionAllowed"] =
                 actionsByDate[len]["actionAllowed"] - 1;
-            actionsBLL.addAction(actionsByDate[len]);
+            await actionsBLL.addAction(actionsByDate[len]);
             next();
         } else {
             res.status(400).json("No more actions for today");
         }
     } catch (err) {
-        res.status(400).json("No more actions for today");
+        res.status(500).json("Internal Error");
     }
 };
 
